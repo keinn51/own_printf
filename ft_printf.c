c30bbc0a8570d686cbc2303767a3bf754bc56aa7
keinn51 <soogood97@gmail.com>
2022-04-09 17:05:21 +0900

    Implement p format for printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -15,65 +16,87 @@
 #include <unistd.h>
 // #include <stdlib.h>
 
-int	printf_putstr_null(char *str)
+int printf_putstr_null(char *str)
 {
-	int	result;
+    int result;
 
-	result = 0;
-	if (!str)
-		str = "(null)";
-	while (*str)
-		result += (int)(write(1, str++, 1));
-	return (result);
+    result = 0;
+    if (!str)
+        str = "(null)";
+    while (*str)
+        result += (int)(write(1, str++, 1));
+    return (result);
 }
 
-int	int_putchar(char c)
+int printf_putchar(char c)
 {
-	int result;
+    int result;
 
-	result = 0;
-	result += (int)(write(1, &c, 1));
-	return (result);
+    result = 0;
+    result += (int)(write(1, &c, 1));
+    return (result);
+}
+
+int print_adr(unsigned long int nb)
+{
+    int len;
+
+    len = 0;
+    if (nb >= 16)
+    {
+        len += print_adr(nb / 16);
+        len += print_adr(nb % 16);
+    }
+    if (nb < 16)
+    {
+        if (nb < 10)
+            len += printf_putchar(nb + 48);
+        else
+            len += printf_putchar(nb + 87);
+    }
+    return (len);
 }
 
 int field_checker(char format, va_list *ap)
 {
-	int result;
+    int result;
 
-	result = 0;
-	if (format == 'c')
-	{
-		result += ft_putchar(va_arg(*ap, int));
-	}
-	else if(format == 's')
-	{
-		ft_putstr_null(va_arg(*ap, char *));
-	}
-	else if (format == 'i')
-	{
-	}
-
-	else if
+    result = 0;
+    if (format == 'c')
+        result += printf_putchar(va_arg(*ap, int));
+    else if (format == 's')
+        result += printf_putstr_null(va_arg(*ap, char *));
+    else if (format == 'p')
+    {
+        result += write(1, "0x", 2);
+        result += print_adr(va_arg(*ap, unsigned long int));
+    }
+    else if (format == 'i' || format == 'd')
+        result += printf_putstr_null(ft_itoa(va_arg(*ap, int)));
 }
 
 int ft_printf(const char *format, ...)
 {
-	va_list ap;
-	int i;
-	int result;
+    va_list ap;
+    int i;
+    int result;
 
-	va_start(ap, format);
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-			result += write(1, &format[i], 1);
-		if (format[i] == '%')
-		{
-			result += field_checker(format[i + 1], &ap);
-			i++;
-		}
-		i++;
-	}
-	va_end(ap);
-	return (result);
+    va_start(ap, format);
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+            result += write(1, &format[i], 1);
+        if (format[i] == '%')
+        {
+            result += field_checker(format[i + 1], &ap);
+            i++;
+        }
+        i++;
+    }
+    va_end(ap);
+    return (result);
+}
+
+int main()
+{
 }
